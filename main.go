7ed1b005c8e64554a16addf833c52e40fab8219b
willofdaedalus <manny.is.daedalus@pm.me>
2024-08-21 17:16:50 +0000

    non-blocking input with a goroutine

--- main.go
+++ main.go
@@ -1,15 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"time"
 	"willofdaedalus/yummychars/serpent"
+
+	"golang.org/x/term"
 )
 
 func main() {
-	s := serpent.InitSnake(1)
+	fmt.Printf("\033[2J\033[H")
+	fd := int(os.Stdin.Fd())
 
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		log.Fatal("error occured putting terminal in raw mode\n")
+	}
+	defer term.Restore(fd, oldState)
+
+	dir := serpent.RIGHT
+	s := serpent.InitSnake(1)
+	s.MoveSnake(dir)
+
+	buf := make([]byte, 1)
 	for {
-		s.MoveSnake(serpent.DOWN)
+		// non-blocking input with a goroutine
+		go func() {
+			_, err := os.Stdin.Read(buf)
+			if err != nil {
+				if os.IsTimeout(err) {
+					return
+				}
+				log.Fatal(err)
+			}
+		}()
+
+		switch buf[0] {
+		case 'w':
+			dir = serpent.UP
+		case 'a':
+			dir = serpent.LEFT
+		case 's':
+			dir = serpent.DOWN
+		case 'd':
+			dir = serpent.RIGHT
+		case 'q':
+			fmt.Printf("\033[2J\033[H")
+			return
+		}
+		s.MoveSnake(dir)
 		s.DrawSnake()
 
 		// add a short sleep to control the loop speed
